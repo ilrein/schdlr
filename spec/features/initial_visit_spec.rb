128f82459cc343a771c67b95f50f51ae664cfa64
Ilia <ilia.reingold@gmail.com>
2014-12-12 09:37:21 -0500

    passing login tests

--- spec/features/initial_visit_spec.rb
+++ spec/features/initial_visit_spec.rb
@@ -1,19 +1,31 @@
 require 'rails_helper'
 
 describe "the signin process", :type => :feature do
-  
+
   let(:user) { create(:user) }
 
   before :each do
   end
 
-  it "signs me in and redirects to the prep page" do
-    visit ("/")
-    within("#login_box") do
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
+  context "signs me in and redirects to the prep page" do
+    it "I can login with a valid email/pw" do
+      visit ("/")
+      within("#login_box") do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+      end
+      click_button 'Sign in'
+      expect(page.current_path).to eq home_preparation_path
     end
-    click_button 'Sign in'
-    expect(page.current_path).to eq home_preparation_path
+    it "I cannot login with an invalid email/pw" do
+      visit ("/")
+      within("#login_box") do
+        fill_in 'user_email', with: "invalid"
+        fill_in 'user_password', with: "invalid"
+      end
+      click_button 'Sign in'
+      expect(page.current_path).to eq ("/")
+    end
   end
+  
 end
